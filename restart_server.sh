6d9411c55401c95c76e87b0402bdfd141f9cf929
huy <dr.huy69@gmail.com>
2022-06-20 18:13:14 +0700

    update

---
+++ restart_server.sh
@@ -0,0 +1,8 @@
+# This script use when run outside docker
+
+kill -15 $(pgrep -f "server.py")
+kill -15 $(pgrep -f "discovery.py")
+export PATH_TO_HOSTS=/etc/hosts
+export PORT=8443
+./venv/bin/python discovery.py 2>&1 &
+./venv/bin/python server.py
