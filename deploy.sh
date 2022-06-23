d13383be1291279e0f999b13f6af6519ad21e548
huy <dr.huy69@gmail.com>
2022-06-24 04:27:58 +0700

    update

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm -r dist
-rsync -av --exclude=extensions_server.db --exclude=venv --exclude=dist --exclude=__pycache__ --exclude=.*  ./* ../extensions_server_deploy/
-pyarmor obfuscate --platform linux.aarch64 server.py
-cp -r ./dist/* ../extensions_server_deploy
-cd ../extensions_server_deploy
-git add .
-git commit -m 'update'
-git push origin master
