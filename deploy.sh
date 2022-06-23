2ed9ef48b3685d803ba924cd51f69ee8fc8ba7f6
huy <dr.huy69@gmail.com>
2022-06-23 08:20:08 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+rm -r dist
 rsync -av --exclude=extensions_server.db --exclude=venv --exclude=dist --exclude=__pycache__ --exclude=.*  ./* ../extensions_server_deploy/
 pyarmor obfuscate --platform linux.aarch64 server.py
 cp -r ./dist/* ../extensions_server_deploy
