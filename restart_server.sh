31d9c19af7a6ede389618a7eb81d5bffdb889a01
huy <dr.huy69@gmail.com>
2022-06-24 05:19:12 +0700

    update

--- restart_server.sh
+++ restart_server.sh
@@ -1,7 +1,5 @@
 # This script use when run outside docker
 cd /root/extensions_server_deploy
-echo "$PWD"
-
 kill -15 $(pgrep -f "server.py")
 kill -15 $(pgrep -f "discovery.py")
 export PATH_TO_HOSTS=/etc/hosts
